87f8868f018c10f11f5f6d504e281e2d3cfd7fd6
felip <felip@ANDRS>
2023-06-20 10:18:22 -0500

    Agregar archivos de proyecto.

---
+++ Sesion1/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("----- Nombre -----");
+        Console.Write("Ingrese su nombre: ");
+        string? nombre = Console.ReadLine();
+        Console.WriteLine("¡Hola, " + nombre + "!");
+
+        Console.WriteLine("----- Hora Actual -----");
+        DateTime horaActual = DateTime.Now;
+        Console.WriteLine($"La hora actual es la siguiente {horaActual:HH:mm:ss}");
+    }
+}
